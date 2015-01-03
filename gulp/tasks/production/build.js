31882f8f04a7f32cd30fab55dcfe3e153ce078da
rober <rmh.dev@gmail.com>
2015-01-03 01:33:48 +0100

    Use production values

--- gulp/tasks/production/build.js
+++ gulp/tasks/production/build.js
@@ -4,7 +4,7 @@ var runSequence = require('run-sequence');
 gulp.task('build:production', function(callback) {
     runSequence(
         'delete',
-        'jekyll', //TODO: don't use development task
+        'jekyll:production',
         'sass',
         'optimize:css',
         callback
--- gulp/tasks/production/jekyll.js
+++ gulp/tasks/production/jekyll.js
@@ -2,7 +2,7 @@ var gulp        = require('gulp');
 var cp          = require('child_process');
 var config      = require('../../config').jekyll.production;
 
-gulp.task('jekyll', function(done) {
+gulp.task('jekyll:production', function(done) {
     return cp.spawn('bundle', ['exec', 'jekyll', 'build', '-q', '--source=' + config.src, '--destination=' + config.destination, '--config=' + config.config], { stdio: 'inherit' })
         .on('close', done);
 });
