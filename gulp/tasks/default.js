908d476ab93806452b76d6aeb6f001623b129946
rober <rmh.dev@gmail.com>
2015-01-24 18:51:36 +0100

    Move css files after sass change

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
 var config = require('../config').watch;
+var runSequence = require('run-sequence');
 
 gulp.task('default', ['browser-sync'], function() {
-    gulp.watch(config.sass, ['sass']);
+    gulp.watch(config.sass, function () {
+        runSequence('sass', 'optimize:development');
+    });
     gulp.watch(config.javascript, ['javascript']);
     gulp.watch(config.images, ['images']);
     gulp.watch(config.jekyll, ['jekyll:sync']);
